6871d1a76d75d7960fedbf16067797b748dc9882
florisbechger <69296716+florisbechger@users.noreply.github.com>
2024-04-12 13:21:46 +0200

    Add files via upload

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -39,7 +39,7 @@
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
   #networking.interfaces.enp86s0.useDHCP = lib.mkDefault false;
-  #networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
+  networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
