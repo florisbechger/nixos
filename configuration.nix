96edc82eb0fc2fd68947ee5f467f05ff9867233b
florisbechger <69296716+florisbechger@users.noreply.github.com>
2026-04-21 16:01:12 +0200

    Add files via upload

---
+++ configuration.nix
@@ -0,0 +1,37 @@
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      ./modules/laptop.dhcp.nix
+      ./modules/laptop.nix
+      ./modules/p43s.nix
+      ./modules/boot-esp.nix
+      ./modules/bluetooth.nix
+      ./modules/clamav.nix
+      ./modules/firewall.nix
+      ./modules/print.nix
+      ./modules/alsa.nix
+      ./modules/gnome.nix
+      ./modules/packages.nix
+      ./modules/applications.nix
+      ./modules/services.nix
+      ./modules/kvm.nix
+      ./modules/flatpak.nix
+      ./modules/flakes.nix
+      ./modules/mega.nix
+    ];
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.floris = {
+    isNormalUser = true;
+    description = "Floris";
+    extraGroups = [ "audio" "networkmanager" "wheel" "libvirtd" ];
+    packages = with pkgs; [
+    # some package
+    ];
+  };
+
+}
