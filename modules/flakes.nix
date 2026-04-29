8abece4d9e6eeb4de8f5fe5cf3496b8c14e68599
florisbechger <69296716+florisbechger@users.noreply.github.com>
2026-04-29 17:58:15 +0000

    Delete modules directory

--- modules/flakes.nix
+++
@@ -1,9 +0,0 @@
-
-{ ... }:
-
-{
-
-  # Enable Flakes.
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-}
--- modules/vm.hostname.nix
+++
@@ -1,9 +0,0 @@
-
-{ config, ... }:
-
-{
-
-  # Hostname.
-  networking.hostName = "vm";
-
-}
