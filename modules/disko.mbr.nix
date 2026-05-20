f9fa38e57f56a7f3f0197c42a55f546a43005dac
thomfele <69296716+thomfele@users.noreply.github.com>
2026-05-20 10:45:26 +0200

    Update disko.mbr.nix

--- modules/disko.mbr.nix
+++ modules/disko.mbr.nix
@@ -6,8 +6,7 @@
   disko.devices = {
     disk = {
       disk0 = {
-        device = "/dev/vda"; # First disk on VM, use MBR
-        #device = "/dev/sda"; # First disk on NUC, use ESP
+        device = "/dev/vda";
         type = "disk";
         content = {
           type = "gpt";
@@ -15,7 +14,7 @@
             boot = {
               type = "EF02"; # for grub MBR
               size = "1M";
-              priority = 1; # Needs to be first partition
+              priority = 1;
             };
             root = {
               size = "100%";
